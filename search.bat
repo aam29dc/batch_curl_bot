cac86852720851e175acbecd2dd36a76e4f55cbc
Michael Arrotta <73267302+aam29dc@users.noreply.github.com>
2024-12-02 22:04:35 -0500

    Update search.bat

--- search.bat
+++ search.bat
@@ -2,11 +2,29 @@
 setlocal EnableDelayedExpansion
 color 02
 
-:: Initialize the file variable
-set file=words.txt
+:: VARIABLES
+:: Browser settings to simulate a real Firefox user
+set brow="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0"
+set type="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+set enc="Accept-Encoding: gzip, deflate, br"
+set lang="Accept-Language: en-US,en;q=0.9"
+set ka="Connection: keep-alive"
+set sec="Upgrade-Insecure-Requests: 1"
+set te="TE: Trailers"
+:: Origin headers to simulate a real browser request
+set origin_google="Origin: https://www.google.com"
+set origin_bing="Origin: https://www.bing.com"
+set origin_yahoo="Origin: https://www.yahoo.com"
+set origin_duckduckgo="Origin: https://www.duckduckgo.com"
+:: Initial Referer (Starting with Google)
+set referer="Referer: https://www.google.com/"
+:: Initial prev_engine
+set prev_engine=
 
-:getfile
+::WORDS file
 :: Check if the file exists
+:getfile
+set file=words.txt
 if not exist "%file%" (
     echo %file% does not exist. Please enter the words filename: 
     set /p file=
@@ -14,10 +32,9 @@ if not exist "%file%" (
     if not exist "%file%" (
         goto getfile
     )
-	
-	echo file
 )
 
+::LOGGING enable/disable
 :getlogging
 echo Enable logging to response.log? (Y/N):
 set /p logging=
@@ -31,44 +48,24 @@ if /i "%logging%" == "Y" (
 	goto :getlogging
 ) 
 
+::SHUFFLE the words
 :: Check if shuffle.ps1 exists
 if not exist "shuffle.ps1" (
     echo shuffle.ps1 does not exist.
     pause
     exit /b
 )
-
 echo Shuffling %file% with shuffle.ps1, please wait...
 powershell -ExecutionPolicy Bypass -File "shuffle.ps1" "%file%"
 echo Shuffle of %file% complete.
 echo.
 
-:: Browser settings to simulate a real Firefox user
-set brow="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0"
-set type="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
-set enc="Accept-Encoding: gzip, deflate, br"
-set lang="Accept-Language: en-US,en;q=0.9"
-set ka="Connection: keep-alive"
-set sec="Upgrade-Insecure-Requests: 1"
-set te="TE: Trailers"
-
-:: Origin headers to simulate a real browser request
-set origin_google="Origin: https://www.google.com"
-set origin_bing="Origin: https://www.bing.com"
-set origin_yahoo="Origin: https://www.yahoo.com"
-set origin_duckduckgo="Origin: https://www.duckduckgo.com"
-
-:: Initial Referer (Starting with Google)
-set referer="Referer: https://www.google.com/"
-:: Initial prev_engine
-set prev_engine=
-
-goto :Main
-:: Main loop to read the search terms and send requests
+::MAIN function
 :Main
 :: Read the words.txt file line by line
 for /f "delims=" %%a in (%file%) do (
     set search=%%a
+	set query=
 
     :: Recalculate random engine selection inside the loop
     set /a random_engine=!random! %% 4
@@ -76,36 +73,16 @@ for /f "delims=" %%a in (%file%) do (
     :: Check which engine to use based on the random selection
     if !random_engine! == 0 (
 		if !prev_engine! neq 0 (
-			:: Perform the curl request for Google
-			for /f "delims=" %%b in ('curl -L -A %brow% -H %type% -H %enc% -H %lang% -H %ka% -H %sec% -H !referer! -H %te% -H %origin_google% -s -w "%%{http_code} %%{time_total}s %%{url_effective}\n" -o NUL "https://www.google.com/"') do (
-				set result=%%b
-				set result=!result:https://www.=!
-				set result=!result:.com/=!
-
-				echo !result!
-				
-				if /i "%logging%" == "Y" (
-					echo !result! >> response.log
-				)
-			)
+			:: Perform the curl request for Google home
+			call:curl google
 			
 			:: Update the referer for search query
 			set referer="Referer: https://www.google.com/"
 		)
 
         :: Perform the curl request for Google search query
-        for /f "delims=" %%b in ('curl -L -A %brow% -H %type% -H %enc% -H %lang% -H %ka% -H %sec% -H !referer! -H %te% -H %origin_google% -s -w "%%{http_code} %%{time_total}s %%{url_effective}\n" -o NUL "https://www.google.com/search?q=!search!"') do (
-            set result=%%b
-            set result=!result:https://www.=!
-            set result=!result:.com/=!
-
-            echo !result!
-
-				
-			if /i "%logging%" == "Y" (
-				echo !result! >> response.log
-			)
-	)
+		set query="search?q=!search!"
+		call:curl google !query!
 
         :: Update the referer for the next search engine
         set referer="Referer: https://www.google.com/search?q=!search!"
@@ -113,35 +90,16 @@ for /f "delims=" %%a in (%file%) do (
 
     ) else if !random_engine! == 1 (
 		if !prev_engine! neq 1 (
-			:: Perform the curl request for Bing
-			for /f "delims=" %%b in ('curl -L -A %brow% -H %type% -H %enc% -H %lang% -H %ka% -H %sec% -H !referer! -H %te% -H %origin_bing% -s -w "%%{http_code} %%{time_total}s %%{url_effective}\n" -o NUL "https://www.bing.com/"') do (
-				set result=%%b
-				set result=!result:https://www.=!
-				set result=!result:.com/=!
-
-				echo !result!
-				
-				if /i "%logging%" == "Y" (
-					echo !result! >> response.log
-				)
-			)
+			:: Perform the curl request for Bing home
+			call:curl bing
 			
 			:: Update the referer for search query
 			set referer="Referer: https://www.bing.com/"
 		)
 
         :: Perform the curl request for Bing search query
-        for /f "delims=" %%b in ('curl -L -A %brow% -H %type% -H %enc% -H %lang% -H %ka% -H %sec% -H !referer! -H %te% -H %origin_bing% -s -w "%%{http_code} %%{time_total}s %%{url_effective}\n" -o NUL "https://www.bing.com/search?q=!search!"') do (
-            set result=%%b
-            set result=!result:https://www.=!
-            set result=!result:.com/=!
-
-            echo !result!
-			
-			if /i "%logging%" == "Y" (
-				echo !result! >> response.log
-			)
-        )
+		set query="search?q=!search!"
+		call:curl bing !query!
 
         :: Update the referer for the next search engine
         set referer="Referer: https://www.bing.com/search?q=!search!"
@@ -149,35 +107,16 @@ for /f "delims=" %%a in (%file%) do (
 
     ) else if !random_engine! == 2 (
 		if !prev_engine! neq 2 (
-			:: Perform the curl request for Yahoo
-			for /f "delims=" %%b in ('curl -L -A %brow% -H %type% -H %enc% -H %lang% -H %ka% -H %sec% -H !referer! -H %te% -H %origin_yahoo% -s -w "%%{http_code} %%{time_total}s %%{url_effective}\n" -o NUL "https://www.yahoo.com/"') do (
-				set result=%%b
-				set result=!result:https://www.=!
-				set result=!result:.com/=!
-
-				echo !result!
-				
-				if /i "%logging%" == "Y" (
-					echo !result! >> response.log
-				)
-			)
+			:: Perform the curl request for Yahoo home
+			call:curl yahoo
 
 			:: Update the referer for search query
 			set referer="Referer: https://www.yahoo.com/"
 		)
 
         :: Perform the curl request for Yahoo
-        for /f "delims=" %%b in ('curl -L -A %brow% -H %type% -H %enc% -H %lang% -H %ka% -H %sec% -H !referer! -H %te% -H %origin_yahoo% -s -w "%%{http_code} %%{time_total}s %%{url_effective}\n" -o NUL "https://search.yahoo.com/search;?p=!search!&fr=sfp&fr2=sb-top"') do (
-            set result=%%b
-            set result=!result:https://search.=!
-            set result=!result:.com/=!
-
-            echo !result!
-			
-			if /i "%logging%" == "Y" (
-				echo !result! >> response.log
-			)
-        )
+		set query="search;?p=!search!&fr=sfp&fr2=sb-top"
+		call:curl yahoo !query!
 
         :: Update the referer for the next search engine
         set referer="Referer: https://search.yahoo.com/search;?p=!search!&fr=sfp&fr2=sb-top"
@@ -185,35 +124,16 @@ for /f "delims=" %%a in (%file%) do (
 
     ) else if !random_engine! == 3 (
 		if !prev_engine! neq 3 (
-			:: Perform the curl request for DuckDuckGo
-			for /f "delims=" %%b in ('curl -L -A %brow% -H %type% -H %enc% -H %lang% -H %ka% -H %sec% -H !referer! -H %te% -H %origin_duckduckgo% -s -w "%%{http_code} %%{time_total}s %%{url_effective}\n" -o NUL "https://www.duckduckgo.com"') do (
-				set result=%%b
-				set result=!result:https://=!
-				set result=!result:.com/=!
-
-				echo !result!
-				
-				if /i "%logging%" == "Y" (
-					echo !result! >> response.log
-				)
-			)
+			:: Perform the curl request for DuckDuckGo home
+			call:curl duckduckgo
 
 			:: Update the referer for search query
 			set referer="Referer: https://duckduckgo.com"
 		)
 
         :: Perform the curl request for DuckDuckGo
-        for /f "delims=" %%b in ('curl -L -A %brow% -H %type% -H %enc% -H %lang% -H %ka% -H %sec% -H !referer! -H %te% -H %origin_duckduckgo% -s -w "%%{http_code} %%{time_total}s %%{url_effective}\n" -o NUL "https://www.duckduckgo.com?q=!search!&ia=web"') do (
-            set result=%%b
-            set result=!result:https://=!
-            set result=!result:.com/=!
-
-            echo !result!
-			
-			if /i "%logging%" == "Y" (
-				echo !result! >> response.log
-			)
-        )
+		set query="?q=!search!&ia=web"
+		call:curl duckduckgo !query!
 
         :: Update the referer for the next search engine
         set referer="Referer: https://www.duckduckgo.com?q=!search!&ia=web"
@@ -222,7 +142,31 @@ for /f "delims=" %%a in (%file%) do (
 )
 
 goto Main
-::end Main
+::end Main - loops infinitely
+
+::curl FUNCTION
+:curl
+
+::setup search query string, if its a homepage or not
+if [%2]==[] ( set str=https://www.%1.com/
+) else ( set str=https://www.%1.com/%2 )
+	for /f "delims=" %%b in ('curl -L -A %brow% -H %type% -H %enc% -H %lang% -H %ka% -H %sec% -H !referer! -H %te% -H %origin_google% -s -w "%%{http_code} %%{time_total}s %%{url_effective}\n" -o NUL %str%') do (
+		set result=%%b
+		set result=!result:https://=!
+		set result=!result:search.=!
+		set result=!result:www.=!
+		set result=!result:.com/=!
+
+		echo !result!
+		
+		if /i "%logging%" == "Y" (
+			echo !result! >> response.log
+		)
+	)
+
+goto:end
+::curl END
 
 endlocal
 pause
+:end
